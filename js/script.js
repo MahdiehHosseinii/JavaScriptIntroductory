56c9e1c9225298504c923bcaf91ea56772e5992f
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-02-17 19:42:56 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,486 @@
+// var usercount=4175
+// var courses=13
+// var minutes=54834
+// var myage=23
+// var myname="mahdieh"
+// var caption= "این دوره برای یادگیری جاوا اسکریپت است"
+// var isonline= true
+
+//این متن برای یادگیری کامنت گذاری است
+
+// برای
+// کامنت
+// است
+
+/*
+کامنت
+ */
+
+// var userinfo=prompt("لطفا نام و نام خانوادگی خود را وارد کنید" , "مهدیه حسینی" )
+// var userAge= prompt("سن خودرا وارد کنید")
+// var usernumberage= +Number
+// alert( typeof usernumberage)
+
+// var userAge=19
+// var userStringAge= String(userAge)
+// alert( typeof userStringAge)
+
+// var firstNumber=17
+// var secondNumber=4
+// var thirdNumber=2
+// var plusResult=firstNumber+secondNumber
+// var minusResult=firstNumber-secondNumber
+// var taghsim=firstNumber/secondNumber
+// var zarb=secondNumber*thirdNumber
+// var tavan=thirdNumber**firstNumber
+// var baghimande=firstNumber%thirdNumber
+// alert (baghimande)
+
+// var number1=prompt("عدد اول")
+// var number2=prompt("عدد دوم")
+// var number1typeNumber= +number1
+// var number2typeNumber= +number2
+// var plusresult=number1typeNumber+number2typeNumber
+// alert(plusresult)
+
+// var sitName="sabzlearn.ir"
+// var coursName="javascript"
+// alert(sitName +"\n"+ coursName)
+
+// alert("this is \"javascript\" course from" +" "+ "sabzlearn.ir")
+
+// alert("site:sabzlearn.ir" +"\n"+ "course:javascript")
+
+// alert("12"/4)
+// alert("1hhj2"/4)
+// alert("12s"/4)
+
+// var str1="sabzlearn.ir"
+// var str2="javascript"
+// var num1=12
+// var num2=4
+// alert(isNaN(num1+num2))
+
+// var num1=12
+// var num2=3
+// var num3=4
+// var str2="3"
+// var str3="4"
+// alert(num2==num3)
+// alert(num2*num3 == num1)
+// alert(num1 < num3)
+// alert(str2==num2)
+// alert(str2 === str2)
+// alert(str2 === num3)
+
+// var num1=12
+// var num2=3
+// var num3=4
+// if (num2/num3 === num1){
+//     alert("درست است")
+// }else {
+//     alert("درست نیست")
+// }
+
+// var userNumber=prompt("عدد وارد کنید" ,0 )
+// if (userNumber % 2 == 0) {
+//     alert("عدد زوج است")
+// }else {
+//     alert("عدد فرد است")
+// }
+
+// var firstnumber=Number(prompt("عدد اول را وارد کنید"))
+// var secondnumber=Number(prompt("عدد دوم را وارد کنید"))
+// var thirdnumber=Number(prompt("عدد سوم را وارد کنید"))
+// var average=(firstnumber+secondnumber+thirdnumber) /3
+// alert(average)
+
+// var userAge=Number(prompt("لطفا سن خود را وارد کنید:"))
+// if (userAge<18) {
+//     alert("شما مجاز به ورود نمیباشید")
+// }else {
+//     alert("شما مجاز به ورود میباشید")
+// }
+
+// var num1 = prompt("عدد اول را وارد کنید" ,0 )
+// var num2 = prompt("عدد دوم را وارد کنید" ,0 )
+// var powerResult = num1 ** num2
+// alert(powerResult)
+
+// var userBirthdate = prompt( "سال تولد خود را وارد کنید:" ,1310)
+// var userAge = 1402 - userBirthdate
+// if (isNaN(userAge)){
+//     alert("مقدار وارد شده صحیح نمی باشد")
+// }else {
+//     alert("سن شما:" +userAge )
+// }
+// if (userBirthdate>1402){
+//     alert("مقدار وارد شده صحیح نمی باشد")
+// }else {
+//     alert("سن شما:" +userAge )
+// }
+
+// var minutes=prompt("زمان خود را وارد کنید(به دقیقه)" , 0)
+// var minutestoHours= minutes / 60
+// if (isNaN(minutestoHours)){
+//     alert("مقدار وارد شده صحیح نمیباشد")
+// }else {
+//     alert(minutestoHours + "Hours")
+// }
+
+// var num1=12
+// var num2=3
+// var num3=4
+// if (num2 * num3 === 10){
+//     alert("ضرب برابر با 10")
+// }else if (num2 * num3 === 11){
+//     alert("ضرب برابر با 11")
+// }else if (num2 * num3 === 13){
+//     alert("ضرب برابر با 12")
+// }else {
+//     alert("نتیجه درست نیست")
+// }
+
+// var num1=12
+// var num2=3
+// var num3=4
+// if (num2 * num3 === 10 || num2 * num3 === 11){
+//     alert("10 یا 11")
+// }else if (num2 * num3 === 13 || num2 * num3 ===14){
+//     alert("13 یا 14")
+// }else if (num2 * num3 === 12 || num2 * num3 ===15){
+//     alert("12 یا 15")
+// }
+
+// var userAge=prompt("سن خود را وارد کنید")
+// var userSex=prompt("جنسیت خود را وارد کنید (زن , مرد)")
+// if (userSex === "زن" || userSex < 18){
+//     alert("خطا")
+// }else {
+//     alert("ورود به پنل")
+// }
+
+// var num1=4
+// var num2=3
+// if (num1===4 && num2===3){
+//     alert("صحیح")
+// }else if (num1 === num2 && num1*num2===12){
+//     alert("غلط")
+// }
+
+// var Bool1=true
+// alert("Bool1 = "  + Bool1)
+// alert("!Bool1 = "  + !Bool1)
+
+// var num1=4
+// var num2=3
+// num1 * num2 === 19 ? alert("برابر با 12 است") : alert("برابر با 19 نیست")
+
+// var num1=3
+// var num2=4
+// var num3= num1 * num2
+// switch (num3) {
+//     case 10:
+//         alert("برابر با 10");
+//         break;
+//     case 11:
+//         alert("برابر با 11");
+//         break;
+//     case 12:
+//         alert("برابر با 12");
+//         break;
+//     default :
+//         alert("برابر با هیچ کدام");
+// }
+
+// var userAverage =Number(prompt("معدل خود را وارد کنید"))
+// switch (userAverage) {
+//     case 18 :
+//     case 19 :
+//     case 20 :
+//         alert("سطح شما A")
+//         break
+//     case 15 :
+//     case 16 :
+//     case 17 :
+//         alert("سطح شما B")
+//         break
+//     case 12 :
+//     case 13 :
+//     case 14 :
+//         alert("سطح شما C")
+//         break
+//     default :
+//         alert("مشروط")
+// }
+
+// function showValue () {
+//     var num1=4
+//     var num2=3
+//     var resultNumber= num1+num2
+//     alert("Result" + resultNumber)
+// }
+// showValue()
+// showValue()
+
+// var showValue = function () {
+//     var num1=3
+//     var num2= 2
+//     var resultNumber= num1 + num2
+//     alert("num1+num2=" + resultNumber)
+// }
+
+// var sayhi=function (){
+//     alert("hi")
+// }
+// sayhi()
+
+// function showTowNumbers (firstNumber , secondNumber) {
+//     alert( "اولین عدد" + firstNumber)
+//     alert( "دومین عدد" + secondNumber)
+// }
+// showTowNumbers(19 , 22)
+
+// function IsEvenOrOdd (number) {
+//    if (number % 2 ===0){
+//        alert(number + "زوج است")
+//    }else {
+//       alert(number + "فرد است")
+//    }
+// }
+// IsEvenOrOdd(23)
+// IsEvenOrOdd(46)
+// IsEvenOrOdd(19)
+// IsEvenOrOdd(68)
+// IsEvenOrOdd(11)
+
+// function AverageCalculator (num1 , num2 , num3) {
+//     var PlusResult= num1 + num2 + num3
+//     var Average=PlusResult / 3
+//     alert("میانگین " + Average)
+// }
+// AverageCalculator(43 , 17 , 35)
+
+// function sumValue (num1=1 , num2=2) {
+//     var result= num1 + num2
+//     alert("Result: " + result)
+// }
+// sumValue()
+
+// var number1= prompt("عدد اول را وارد کنید")
+// var number2= prompt("عدد دوم را وارد کنید")
+// function ShowPower (num1 , num2) {
+//     return num1 ** num2
+// }
+// var PowerResult= ShowPower(number1 , number2)
+// alert(" جواب توان " + PowerResult)
+
+// var userName= "mahdiehhosseini"
+// alert(userName[9])
+// alert(userName.length)
+
+// var userName= prompt("نام کاربری خود را وارد کنید")
+// var password= prompt("رمز خود را وارد کنید")
+// if (userName.length < 3 || password.length< 8 ) {
+//     alert("نام کاربری شما باید بیشتر از 3 و پسورد شما باید بیشتر از 8 کاراکتر داشته باشد")
+// }else {
+//     alert("شما با موفقیت وارد شدید")
+// }
+
+// var text = "I Love Javascript     "
+// console.log(text.charAt(2))
+// console.log(text.charCodeAt(2))
+// console.log(text + " & React")
+// console.log(text.concat("&React"))
+// console.log(text.trim())
+// console.log(text.toLowerCase())
+// console.log(text.toUpperCase())
+// console.log(text.search("Love"))
+// console.log(text.indexOf("Love"))
+// console.log(text.includes("Love"))
+
+// var userName= prompt("Enter your username")
+// if (userName.toLowerCase() === "ali") {
+//     alert("شما با موفقیت وارد شدید")
+// } else {
+//     alert("شما مجار به ورود نیستید")
+// }
+
+// var text = "I Love Javascript & React"
+// console.log(text.slice(2 , 6))
+// console.log(text.substr(2 , 4))
+// console.log(text.substring(2 , 6))
+
+// console.log(Math.PI)
+// console.log(Math.pow(2 , 3))
+// console.log(Math.sqrt(81))
+// console.log(Math.abs(-19))
+// console.log(Math.min(19 , 20 , 90 , 34 , 8))
+// console.log(Math.max(19 , 20 , 90 , 34 , 8))
+// console.log(Math.floor(19.28))
+// console.log(Math.trunc(19.28))
+// console.log(Math.round(19.80))
+// console.log(Math.ceil(19.80))
+// console.log(Math.random())
+// console.log(Math.random() * 20)
+
+// var FirstRandomNumber = Math.random() *100000
+// var FinalRandomNumber = Math.floor(FirstRandomNumber)
+// console.log("your capcha code: " , FinalRandomNumber)
+
+// var number = 10
+// number+=6
+// number-=6
+// number*=2
+// number/=5
+// number**=3
+// ++number
+// number++
+// --number
+// number--
+// console.log(number)
+
+// var num1= 0
+// var num2=num1++
+// var num2=++num1
+// console.log(num1)
+// console.log(num2)
+
+// for (var m=0 ; m<5 ; m++) {
+//     alert("yes")
+// }
+
+// var username="Mahdieh Hosseini"
+// for (var i = 0 ; i<username.length ; i++) {
+//     console.log(i ," => ", username[i])
+// }
+
+// for (var i=0 ; i <100 ; i+=2) {
+//     console.log(i)
+// }
+
+// for (var i = 1 ; i < 100 ; i+=2) {
+//     console.log(i)
+// }
+
+// var Allprices = 0
+// for (var i = 0 ; i < 5 ; i++) {
+//     Allprices= Allprices+Number(prompt("قیمت هارا وارد کنید"))
+// }
+// alert("قیمت نهایی : " + Allprices)
+
+ // var userNumber = null
+ // var sum = 0
+ // for (var i = 0 ; i < 5 ; i++) {
+ //     userNumber = Number(prompt("Enter The "  + (i+1) + " Number : " ))
+ //     sum = sum + userNumber
+ // }
+ // console.log("Average: " + (sum / 5))
+
+//  var userNumber = Number(prompt("Enter Your Number" , 0))
+//  var sum = 0
+//  for (var i = 0 ; (userNumber / 10)!=0 ; i++) {
+//      sum = sum + (userNumber % 10)
+//      userNumber= Math.floor(userNumber / 10)
+//  }
+// console.log("sum of digits : " + sum)
+
+// var userNumber= Number(prompt("Enter Your Number : " , 0 ))
+// var counter = 0
+// if (userNumber == 0) {
+//     counter = 1 ;
+// } else {
+//     for (var i = 0 ; (userNumber / 10 != 0) ; i++) {
+//         counter++
+//         userNumber = Math.floor(userNumber / 10)
+//     }
+// }
+// alert("count of your digits: " + counter )
+
+// var i = 0
+// while (i < 100) {
+//     console.log(i)
+//     i +=2
+// }
+
+// var userNumber = null
+// var i = 0
+// var sum = 0
+// while (i < 3) {
+//     userNumber = Number(prompt("Enter your number : "))
+//     sum = sum + userNumber
+//     i++
+// }
+// alert("Average : " + (sum / 3))
+
+// var userNumber = Number(prompt("Enter The Number : " , 0))
+// var sum = 0
+// var count = 0
+// while (userNumber / 10 !=0) {
+//     sum = sum + (userNumber % 10)
+//     count++
+//     userNumber = Math.floor(userNumber / 10)
+// }
+// alert("sum of digits: " + sum + "\n" + "count of digits: " + count )
+
+// var firstNumber = Number(prompt("Enter Your First Number : "))
+// var secondNumber = Number(prompt("Enter Your Second Number : "))
+// if (firstNumber % 2 === 0) {
+//     while (firstNumber < secondNumber) {
+//         console.log(firstNumber)
+//         firstNumber +=2
+//     }
+// }else {
+//     firstNumber ++
+//     while (firstNumber < secondNumber) {
+//         console.log(firstNumber)
+//         firstNumber +=2
+//     }
+// }
+
+// var userNumber = 0
+// var sum = 0
+// var counter = 0
+// while (userNumber != -1) {
+//     sum +=userNumber
+//     userNumber = Number(prompt('Enter The Number: \n if you dont have number ,type -1'))
+//     if (userNumber != -1) {
+//         counter++
+//     }
+// }
+// console.log("Average : " , sum / counter)
+
+// var FirstNumber = Number(prompt("Enter The FirstNumber" , 1))
+// var SecondNumber = Number(prompt("Enter The SecondNumber" , 1))
+// var power = 1
+// for (var i = 0 ; i < SecondNumber ; i++) {
+//     power = power * firstNumber
+// }
+// alert("power: " + power)
+
+// var FirstNumber = Number(prompt("Enter The FirstNumber" , 1))
+// var SecondNumber = Number(prompt("Enter The SecondNumber" , 1))
+// var Power = 1
+// var i = 0
+// while (i < SecondNumber) {
+//     Power = Power * FirstNumber
+//     i++
+// }
+// alert("Power: " + Power)
+
+// var FirstNumber = Number(prompt("Enter The FirstNumber" , 1))
+// var SecondNumber = Number(prompt("Enter The SecondNumber" , 1))
+// var Power = 1
+// var i = 0
+// do {
+//     Power = Power * FirstNumber
+//     i++
+// } while (i < SecondNumber)
+// alert("power: " + Power)
+
+
+
+
+
+
+
