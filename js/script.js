e2fa6966741126ecce78c4de612b361f35849974
Mahdieh Hosseini <mahdieh320@gmail.com>
2024-02-24 22:12:38 +0330

    practice

--- js/script.js
+++ js/script.js
@@ -811,4 +811,38 @@
 //     console.log("یک شکل نیستند")
 // }
 
+// var todosArray = [
+//     {id: 1, title: "learn js", isDoing: false},
+//     {id: 2, title: "make footer", isDoing: true},
+//     {id: 3, title: "design", isDoing: false},
+//     {id: 4, title: "vue js", isDoing: false}
+// ]
+// var userMenu = prompt("choose one of them: \n 1.add to do \n 2.remove todo \n 3.do todo")
+// if (userMenu === "1") {
+//     var newTodoName = prompt("enter the new todo name: ")
+//     var newTodoObject = {
+//         id: todosArray.length + 1,
+//         title: newTodoName,
+//         isDoing: false
+//     }
+//     todosArray.push(newTodoObject)
+//     console.log("todos array:", todosArray)
+// } else if (userMenu === "2") {
+//     var removeTodoName = prompt("enter the removable todo name: ")
+//     var removableTodoIndex = todosArray.findIndex(function (todo) {
+//         return todo.title === removeTodoName
+//     })
+//     todosArray.splice(removableTodoIndex, 1)
+//     console.log("todos array:", todosArray)
+// } else if (userMenu === "3") {
+//     var editableTodoName = prompt("enter the editable todo name: ")
+//     todosArray.forEach(function (todo) {
+//         if (todo.title === editableTodoName) {
+//             todo.isDoing = true
+//         }
+//     })
+//     console.log("todos array:", todosArray)
+// } else {
+//     alert("مقدار وارد شده صحیح نمیباشد")
+// }
 
